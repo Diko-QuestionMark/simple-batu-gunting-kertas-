f5453a3722397953a5bebd8d252811da04937963
Feed <fedrikosetiawanlp@gmail.com>
2026-01-17 14:52:33 +0700

    done v1

--- start.gd
+++ start.gd
@@ -1,16 +1,21 @@
 extends Control
 
-var win = Global.win_counter
-var lose = Global.lose_counter
+
 
 @onready var winLabel = $win
 @onready var loseLabel = $lose
+@onready var versionLabel = $version
 
+var version = 1.0
 
 func _ready() -> void:
 	SaveLoad._load()
+	var win = Global.win_counter
+	var lose = Global.lose_counter
+	
 	winLabel.text = "win: " + str(win)
 	loseLabel.text = "lose: " + str(lose)
+	versionLabel.text = "version " + str(version)
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://simple.tscn")
